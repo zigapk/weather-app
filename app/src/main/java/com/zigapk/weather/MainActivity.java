769d244ed1e1e406fb7e40b11305c6b1981d2117
zigapk <ziga.patacko@gmail.com>
2016-06-23 11:41:04 +0200

    fix imports

--- app/src/main/java/com/zigapk/weather/MainActivity.java
+++ app/src/main/java/com/zigapk/weather/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private static FloatingActionButton fab;
     public static Context context;
     private static CoordinatorLayout coordinatorLayout;
+
+    private static RecyclerView mRecyclerView;
+    private static CitiesListAdapter mAdapter;
+    private static RecyclerView.LayoutManager mLayoutManager;
 
     final public static int CITY_REQUEST_CODE = 0;
     private static boolean noConnectionSnackbarOpen = false;
@@ -42,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.cities_list);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new CitiesListAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        showAndHideElements();
+
     }
 
     @Override
@@ -50,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CITY_REQUEST_CODE){
             String city = data.getStringExtra("city");
             if (!city.equals("")) {
-                // add city to list
+                mAdapter.addItem(city);
+                showAndHideElements();
             }
         }
     }
@@ -71,6 +85,18 @@ public class MainActivity extends AppCompatActivity {
 
             noConnectionSnackbarOpen = true;
             snackbar.show();
+        }
+    }
+
+    private void showAndHideElements(){
+        if (mAdapter.getItemCount() == 0){
+            findViewById(R.id.no_cities_tv).setVisibility(View.VISIBLE);
+            findViewById(R.id.cities_list).setVisibility(View.GONE);
+            findViewById(R.id.swipeRefreshLayout).setVisibility(View.GONE);
+        }else {
+            findViewById(R.id.no_cities_tv).setVisibility(View.GONE);
+            findViewById(R.id.cities_list).setVisibility(View.VISIBLE);
+            findViewById(R.id.swipeRefreshLayout).setVisibility(View.VISIBLE);
         }
     }
 }
